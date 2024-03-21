45993fb932f7e422d30028140e38a307518532af
LuisSebs <sebastian_luis@ciencias.unam.mx>
2024-03-21 14:11:56 -0600

    feat: paso 7 del pdf de la practica, ya abre el modal

--- src/app/module/product/product.module.ts
+++ src/app/module/product/product.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryComponent } from './component/category/category.component';
-
+import  { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -9,7 +9,9 @@ import { CategoryComponent } from './component/category/category.component';
     CategoryComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class ProductModule { }
