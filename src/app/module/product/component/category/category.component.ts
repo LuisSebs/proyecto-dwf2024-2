6591277a7cc0f4b73465b9a05a01bdfd6d220eaf
edulol-1 <edu3000@ciencias.unam.mx>
2024-03-22 19:27:21 -0600

    Feat: Define form para componente categoria, asi como su comportamiento al mandar informacion y cerrar el formulario

--- src/app/module/product/component/category/category.component.ts
+++ src/app/module/product/component/category/category.component.ts
@@ -16,9 +16,18 @@ export class CategoryComponent {
 
   categories: Category[] = [];
 
+  // Category form
+  checkoutForm = this.formBuilder.group({
+    category: ["", [Validators.required]],
+    acronym: ["", [Validators.required]], 
+  });
+
+  submitted = false;
+
   constructor(
-    private categoryService: CategoryService
-    ) { }
+    private categoryService: CategoryService,
+    private formBuilder: FormBuilder,
+  ) { }
 
   getCategories() {
     this.categories = this.categoryService.getCategories();
@@ -36,5 +45,46 @@ export class CategoryComponent {
     $("#modalForm").modal("hide");
   }
 
+  onSubmit(): void {
+    // Validamos el form
+    this.submitted = true;
+    if (this.checkoutForm.invalid) {
+      Swal.fire({
+        position: 'top-end',
+	icon: 'error',
+	toast: true,
+	text: 'Input invalido',
+	background: '#E8F8F8',
+	showConfirmationButton: false,
+	timer: 3000
+      });
+      return;
+    }
+    this.submitted = false;
+    // Agregamos la categoria en la lista de categorias
+    let id = this.categories.length + 1;
+    let categoria = new Category(id, this.checkoutForm.controls['category'].value!,
+				 this.checkoutForm.controls['acronym'].value!, 1);
+    this.categories.push(categoria);
 
+    // Escondemos el form
+    this.hideModalForm();
+    this.checkoutForm.reset();
+
+    // Mostramos el mensaje de exito
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      toast: true,
+      text: 'La nueva categoria ha sido agregada',
+      background: '#E8F8F8',
+      showConfirmationButton: false,
+      timer: 3000
+    });
+  }
+
+  onClose() { 
+    this.hideModalForm();
+    this.checkoutForm.reset();
+  }
 }
