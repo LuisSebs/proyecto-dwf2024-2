d0be5afc0d3b81837436e1a774a25092ec513b45
LuisSebs <sebastian_luis@ciencias.unam.mx>
2024-04-08 20:43:26 -0600

    feat: parte3 3 y 4 de la practica implementada, navbar agregado

--- src/app/module/product/_service/category.service.ts
+++ src/app/module/product/_service/category.service.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { api_dwb_uri } from '../../../shared/uri/api-dwb-uri';
 import { Observable } from 'rxjs';
 import { ApiResponse } from '../../commons/_dto/api-response';
+import { ThisReceiver } from '@angular/compiler';
 
 
 @Injectable({
@@ -18,8 +19,31 @@ export class CategoryService {
     private http: HttpClient
   ) { }
 
-  getCategories(): Observable<HttpResponse<Category[]>>{
+  getCategories(): Observable<HttpResponse<Category[]>> {
     return this.http.get<Category[]>(api_dwb_uri + this.source, { observe: 'response' });
   }
 
+  getCategory(id: number): Observable<HttpResponse<Category>> {
+    return this.http.get<Category>(api_dwb_uri + this.source + "/" + id, { observe: 'response' });
+  }
+
+  getActiveCategories(): Observable<HttpResponse<Category[]>> {
+    return this.http.get<Category[]>(api_dwb_uri + this.source + "/active", { observe: 'response' });
+  }
+
+  createCategory(category: any): Observable<HttpResponse<ApiResponse>> {
+    return this.http.post<ApiResponse>(api_dwb_uri + this.source, category, { observe: 'response' });
+  }
+
+  updateCategory(category: any, id: number): Observable<HttpResponse<ApiResponse>> {
+    return this.http.put<ApiResponse>(api_dwb_uri + this.source + "/" + id, category, { observe: 'response' });
+  }
+
+  deleteCategory(id: number): Observable<HttpResponse<ApiResponse>> {
+    return this.http.delete<ApiResponse>(api_dwb_uri + this.source + "/" + id, { observe: 'response' });
+  }
+
+  activateCategory(id: number): Observable<HttpResponse<ApiResponse>> {
+    return this.http.put<ApiResponse>(api_dwb_uri + this.source + "/" + id + "/activate", null, { observe: 'response' });
+  }
 }
--- src/app/module/product/component/category/category.component.ts
+++ src/app/module/product/component/category/category.component.ts
@@ -54,6 +54,7 @@ export class CategoryComponent {
     $("#modalForm").modal("show");
     this.submitted = false;
     this.checkoutForm.reset();
+    this.categoryToUpdate = 0; // reset categoryToUpdate
   }
 
   hideModalForm(){
@@ -64,37 +65,108 @@ export class CategoryComponent {
     // Validamos el form
     this.submitted = true;
     if (this.checkoutForm.invalid) {
-      Swal.fire({
-        position: 'top-end',
-	icon: 'error',
-	toast: true,
-	text: 'Input invalido',
-	background: '#E8F8F8',
-	showConfirmButton: false,
-	timer: 3000
-      });
+      this.swal.errorMessage('Input invalido');
       return;
     }
     this.submitted = false;
+
+    // validate categoryToUpdate
+    if (this.categoryToUpdate == 0){
+      this.onSubmitCreate();
+    }else{
+      this.onSubmitUpdate();
+    }
+  }
+
+  onSubmitCreate(){
+    console.log(this.checkoutForm.value)
+
     // Agregamos la categoria en la lista de categorias
-    let id = this.categories.length;
-    let categoria = new Category(id, this.checkoutForm.controls['category'].value!,
-				 this.checkoutForm.controls['acronym'].value!, 1);
-    this.categories.push(categoria);
+    this.categoryService.createCategory(this.checkoutForm.value).subscribe({
+      next: (v) => {
+        // Mostramos el mensaje de exito
+        this.swal.successMessage(v.body!.message);
+        // Recargamos las categorias
+        this.getCategories();
+        // Escondemos el form
+        this.hideModalForm();
+        this.checkoutForm.reset();
+      },
+      error: (e) => {
+        console.log(e);
+        this.swal.errorMessage(e.error!.message);
+      }
+    });
+  }
 
-    // Escondemos el form
-    this.hideModalForm();
+  onSubmitUpdate(){
+    // add category to category list
+    this.categoryService.updateCategory(this.checkoutForm.value, this.categoryToUpdate).subscribe({
+      next: (v) => {
+        this.swal.successMessage(v.body!.message);
+        this.getCategories(); // reload categories
+        this.hideModalForm(); // close modal
+        this.categoryToUpdate = 0; // reset categoryToUpdate
+      },
+      error: (e) => {
+        console.log(e);
+        this.swal.errorMessage(e.body!.message); // show message
+      }
+    });
+  }
+
+  updateCategory(category: Category){
+    this.categoryToUpdate = category.category_id;
     this.checkoutForm.reset();
+    this.checkoutForm.controls['category'].setValue(category.category);
+    this.checkoutForm.controls['acronym'].setValue(category.acronym);
+    this.submitted = false;
+    $("#modalForm").modal("show");
+  }
 
-    // Mostramos el mensaje de exito
-    Swal.fire({
-      position: 'top-end',
-      icon: 'success',
-      toast: true,
-      text: 'La nueva categoria ha sido agregada',
-      background: '#E8F8F8',
-      showConfirmButton: false,
-      timer: 3000
+  deleteCategory(id: number){
+    this.swal.confirmMessage.fire({
+      title: 'Favor de confirmar la activación de la región',
+      icon: 'warning',
+      showCancelButton: true,
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Confirmar',
+    }).then((result: any) => {
+      if (result.isConfirmed){
+        this.categoryService.deleteCategory(id).subscribe({
+          next: (v) => {
+            this.swal.successMessage(v.body!.message); // show message
+            this.getCategories(); // reload categories
+          },
+          error: (e) => {
+            console.log(e);
+            this.swal.errorMessage(e.error!.message); // show message
+          }
+        });
+      }
+    });
+  }
+
+  activateCategory(id: number){
+    this.swal.confirmMessage.fire({
+      title: 'Favor de confirmar la activacion de la region',
+      icon: 'warning',
+      showCancelBUtton: true,
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Confirmar',
+    }).then((result: any) => {
+      if (result.isConfirmed){
+        this.categoryService.activateCategory(id).subscribe({
+          next: (v) => {
+            this.swal.successMessage(v.body!.message); // show message
+            this.getCategories(); // reload categories
+          },
+          error: (e) => {
+            console.log(e);
+            this.swal.errorMessage(e.error!.message); // show message
+          }
+        });
+      }
     });
   }
 
