86bb1a32485e5853d16bfe27418771e35e44531b
LuisSebs <sebastian_luis@ciencias.unam.mx>
2024-04-17 20:14:32 -0600

    feat: parte 2 implementada (integracion de productos), falta el detalle del producto

--- src/app/module/layout/app-layout/app-layout.routing.ts
+++ src/app/module/layout/app-layout/app-layout.routing.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { SecuredComponent } from '../../authentication/secured/secured.component';
 import { authenticationGuard } from '../../authentication/_guard/authentication.guard';
+import { ProductComponent } from '../../product/component/product/product/product.component';
 import { CategoryComponent } from '../../product/component/category/category.component';
 import { LoginComponent } from '../../authentication/login/login.component';
 import { RegisterComponent } from '../../authentication/register/register.component';
@@ -11,5 +12,6 @@ export const AppLayoutRoutes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     {path: 'categoria', component: CategoryComponent},
+    {path: 'producto', component: ProductComponent},
     {path: 'secured', component: SecuredComponent, canActivate : [authenticationGuard]}
 ];
--- src/app/module/product/product.module.ts
+++ src/app/module/product/product.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryComponent } from './component/category/category.component';
 import  { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ProductComponent } from './component/product/product/product.component';
 
 
 @NgModule({
   declarations: [
-    CategoryComponent
+    CategoryComponent,
+    ProductComponent
   ],
   imports: [
     CommonModule,
